d687595eebd2cded5a486b94ab1d4d7d3eea6221
Supraja Tangella <supraja.tangella@gmail.com>
2025-07-16 14:54:16 +0100

    Removed unnecessary code

--- ConfigSettings/ConfigSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AzureAIApp.ConfigSettings
-{
-    public class ConfigSettings
-    {
-        public string? SubscriptionKey { get; set; }
-        public string? ServiceRegion { get; set; }
-    }
-}
